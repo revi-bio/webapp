dbc8f889a31ba9195dc2b480030be8ad3edc4185
csokaandras <csokaandras@turr.hu>
2025-02-09 15:07:54 +0100

    Searchbar without function

---
+++ src/components/global/Searchbar.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import Input from '@/components/global/Input.vue';
+  const search = defineModel()
+  /*
+  */
+</script>
+
+<template>
+  <Input class="input" placeholder="Search" v-model="search" type="text"></Input>
+</template>
+
+<style>
+.input{
+  @apply w-80
+}
+</style>
