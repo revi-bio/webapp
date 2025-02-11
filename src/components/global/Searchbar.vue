e3feb38940c3d7bbee9c5ad2aa07518a4896e86b
csokaandras <csokaandras@turr.hu>
2025-02-11 22:26:32 +0100

    RBW-44: search bar is not dynamic

--- src/components/global/Searchbar.vue
+++ src/components/global/Searchbar.vue
@@ -1,12 +1,27 @@
 <script lang="ts" setup>
   import Input from '@/components/global/Input.vue';
   const search = defineModel()
-  /*
-  */
+  const basearray = defineModel<Record<string, unknown>[]>({ default: () => [] });
+
+  const emit = defineEmits<{
+    (event: 'filtered', value: { name: string }[]): void
+  }>();
+
+
+
+  function activateFilter(filter: string | unknown) {
+  if (!Array.isArray(basearray.value)) return;
+
+    const filtered = basearray.value.filter(
+      item => item.name?.toLowerCase().includes((filter as string).toLowerCase()) // `?.` elkerüli a hibát, ha nincs `name`
+    );
+    emit('filtered', filtered);
+  }
+
 </script>
 
 <template>
-  <Input class="input" placeholder="Search" v-model="search" type="text"></Input>
+  <Input class="input" placeholder="Search" v-model="search" @change="activateFilter(search)" type="text"></Input>
 </template>
 
 <style>
--- src/views/Logged_in/Bioslist.vue
+++ src/views/Logged_in/Bioslist.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
   import { ref } from 'vue';
   import Searchbar from '@/components/global/Searchbar.vue';
-import BioListItem from '@/components/global/BioListItem.vue';
+  import BioListItem from '@/components/global/BioListItem.vue';
   const search = ref('');
-  /*
-  */
+  const filteredData = ref<{ name: string, id: string }[]>([]);
+
   const biolists = [
     { id: "1", name: "Lakatos Dezso", domain: "@lakatosdezso", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
     { id: "2", name: "Radics Ferenc", domain: "@radicsferenc", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
@@ -12,12 +12,14 @@ import BioListItem from '@/components/global/BioListItem.vue';
     { id: "4", name: "Olah Peter", domain: "@olahpeter", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
     { id: "5", name: "Rostas Armando", domain: "@rostasarmando", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
   ]
+
+  shoved:[] = biolists
 </script>
 
 <template>
 <div class="w-full h-full flex flex-col space-y-6 p-6 pt-20">
   <div class="w-full rounded-[16px] p-4 bg-zinc-700/50">
-    <Searchbar v-model="search"></Searchbar>
+    <Searchbar v-model="search" :basearray="biolists"></Searchbar>
   </div>
   <div v-for="item in biolists">
     <BioListItem :id="item.id" :name="item.name" :domain="item.domain" :views="item.views" :widgets="item.widgets" :createdat="item.createdat" :lastedit="item.lastedit"></BioListItem>
