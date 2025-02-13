cb35949a305807cdf78eeab44f768bf29c90b625
csokaandras <csokaandras@turr.hu>
2025-02-13 17:49:35 +0100

    WRB-43 searchbar searching

--- src/components/global/Searchbar.vue
+++ src/components/global/Searchbar.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import Input from '@/components/global/Input.vue';
 import type { Bio } from '@/types/Bio';
-import { computed, ref } from 'vue';
+import { watch } from 'vue';
 
 const search = defineModel<string>();
 const props = defineProps<{
@@ -10,28 +10,26 @@ const props = defineProps<{
 
 const emit = defineEmits(["filtered"]);
 
-function activateFilter(filter: string ) {
-  if (filter == "") {
-    emit('filtered', props.basearray)
+watch(search, (newSearch) => {
+  if (!newSearch) {
+    emit('filtered', props.basearray);
+    return;
   }
 
   const filtered = props.basearray.filter(item =>
-    item.name.toLowerCase()
-          .replace(/\s+/g, "")
-          .includes(filter.toLowerCase().replace(/\s+/g, ""))
+    item.name.toLowerCase().replace(/\s+/g, "").includes(newSearch.toLowerCase().replace(/\s+/g, ""))
   );
-  console.log(filtered)
+
   emit('filtered', filtered);
-}
+});
 
 </script>
 
 <template>
-  <Input 
-    class="input" 
-    placeholder="Search" 
+  <Input
+    class="input"
+    placeholder="Search"
     v-model="search"
-    @update:model-value="activateFilter(search!)"
     type="text"
   />
 </template>
--- src/views/Logged_in/Bioslist.vue
+++ src/views/Logged_in/Bioslist.vue
@@ -4,23 +4,26 @@
   import BioListItem from '@/components/global/BioListItem.vue';
 import type { Bio } from '@/types/Bio';
   const search = ref('');
-  const filteredData = ref<Bio[]>([]);
 
-  const biolists:Bio[] = [
+  const biolists = ref<Bio[]>([
     { id: "1", name: "Lakatos Dezso", domain: "@lakatosdezso", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
     { id: "2", name: "Radics Ferenc", domain: "@radicsferenc", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
     { id: "3", name: "Tzinka Jozsef", domain: "@tzinkajozsef", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
     { id: "4", name: "Olah Peter", domain: "@olahpeter", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
     { id: "5", name: "Rostas Armando", domain: "@rostasarmando", views: 420, widgets: 7, createdat: "2025-01-09", lastedit: "2025-02-10" },
-  ]
+  ])
+  const filteredData = ref<Bio[]>([...biolists.value]);
 
+  function changeSearch(filtered:Bio[]){
+    filteredData.value = filtered
+  }
 
 </script>
 
 <template>
 <div class="w-full h-full flex flex-col space-y-6 p-6 pt-20">
   <div class="w-full rounded-[16px] p-4 bg-zinc-700/50">
-    <Searchbar v-model="search" :basearray="biolists" @filtered="!filteredData"></Searchbar>
+    <Searchbar v-model="search" :basearray="biolists" @filtered="changeSearch"></Searchbar>
   </div>
   <div v-for="item in filteredData">
     <BioListItem :id="item.id" :name="item.name" :domain="item.domain" :views="item.views" :widgets="item.widgets" :createdat="item.createdat" :lastedit="item.lastedit"></BioListItem>
