f1dace9b462d06f8b7fc6c82b3cab5669b132db6
csandrisTurr <csaszarandras@turr.hu>
2025-04-13 01:05:52 +0200

    feat: added sidebars to editor + new editor layout

--- src/views/Logged_in/Editor.vue
+++ src/views/Logged_in/Editor.vue
@@ -8,16 +8,32 @@ import { GENERIC_SETTINGS_DEFINITIONS, SPECIFIC_SETTINGS_DEFINITIONS, WidgetGene
 import Widget from '@/components/widget/Widget.vue';
 import { v4 as uuidv4 } from 'uuid';
 import Input from '@/components/global/Input.vue';
+import Icon from '@/components/global/Icon.vue';
 
 const route = useRoute();
 const id = route.params.id;
-const visible = ref(true);
 // const selectedWidgetId: Ref<string | null> = ref(null);
 // const selectedWidgetIndex = computed(() => widgetList.value.findIndex(x => x.id == selectedWidgetId.value))
 const selectedWidgetIndex: Ref<number | null> = ref(null);
+const widgetToolboxOpened = ref(false);
+const bioSettingsOpened = ref(false);
 
 function selectWidget(index: number) {
   selectedWidgetIndex.value = index == selectedWidgetIndex.value ? null : index;
+  widgetToolboxOpened.value = false;
+  bioSettingsOpened.value = false;
+}
+
+function openWidgetToolbox() {
+  selectedWidgetIndex.value = null;
+  widgetToolboxOpened.value = !widgetToolboxOpened.value;
+  bioSettingsOpened.value = false;
+}
+
+function openBioSettings() {
+  selectedWidgetIndex.value = null;
+  widgetToolboxOpened.value = false;
+  bioSettingsOpened.value = !bioSettingsOpened.value;
 }
 
 const widgetList: Ref<IWidget[]> = ref([
@@ -54,39 +70,90 @@ const widgetList: Ref<IWidget[]> = ref([
     type: 'link',
   },
 ]);
+
+const pages = ref([...new Set(widgetList.value.map(x => x.page))]);
+const currentPageIndex = ref(0);
+
+function goLeft() {
+  if (currentPageIndex.value == 0) return;
+
+  if (!widgetList.value.find(x => x.page == currentPageIndex.value))
+    pages.value.pop();
+
+  currentPageIndex.value--;
+}
+
+function goRight() {
+  if (pages.value.length == 3) return;
+
+  if (pages.value.includes(currentPageIndex.value + 1)) {
+    currentPageIndex.value++;
+  } else {
+    pages.value.push(currentPageIndex.value + 1);
+    currentPageIndex.value++;
+  }
+}
 </script>
 
 <template>
   <div class="flex justify-center items-center h-full w-full relative">
-    <div class="absolute left-0 bottom-0 p-6 flex space-x-2 text-zinc-200">
-      <Button icon-position="only" icon-type="eye_tracking" size="small" rank="secondary"
-        @click.prevent="visible = !visible"></Button>
-      <Button icon-position="only" icon-type="info" size="small" rank="secondary"
-        v-if="visible"></Button>
-      <Button icon-position="right" icon-type="delete" size="small" rank="secondary" text="Clear all"
-        v-if="visible"></Button>
-    </div>
-
-    <div v-if="selectedWidgetIndex != null && visible"
-      class="absolute right-0 top-1/2 transform -translate-y-1/2 pr-4 items-center z-10 flex flex-col">
-      <div class="flex flex-col bg-zinc-800/50 p-4">
-        <Input v-for="setting in SPECIFIC_SETTINGS_DEFINITIONS[widgetList[selectedWidgetIndex].type]" type="text"
-          v-model="widgetList[selectedWidgetIndex].specificSettings[setting.name]"></Input>
+    <div class="absolute w-full bottom-0 p-6 justify-between space-x-2 text-zinc-200 grid grid-cols-3">
+      <div class="flex gap-2">
+        <Button :onClick="openWidgetToolbox" icon-position="only" icon-type="add" size="small" rank="primary" />
+        <Button icon-position="only" icon-type="delete" size="small" rank="secondary" :disabled="selectedWidgetIndex == null"/>
       </div>
-      <div class="flex flex-col bg-zinc-800/50 p-4">
-        <Input v-for="setting in GENERIC_SETTINGS_DEFINITIONS" type="text"
-          v-model="widgetList[selectedWidgetIndex].genericSettings[setting.name]"></Input>
+      <div class="flex gap-2 items-center justify-center">
+        <Button :onClick="goLeft" icon-position="only" icon-type="chevron_left" size="small" rank="secondary" />
+        <span v-for="page in pages" :key="page">{{ pages[currentPageIndex] == page ? 'x' : '-' }}</span>
+        <Button :onClick="goRight" icon-position="only" icon-type="chevron_right" size="small" rank="secondary" />
       </div>
-      <!--
-        <Toolbar />
-      -->
+      <div class="flex justify-end">
+        <Button :onClick="openBioSettings" icon-position="only" icon-type="settings" size="small" rank="secondary"/>
+      </div>
     </div>
-
+      <Teleport defer to="#sidebar-right-outlet" v-if="widgetToolboxOpened">
+        <div class="sidebar">
+          <span class="text-2xl">Widget toolbox</span>
+        </div>
+      </Teleport>
+      <Teleport defer to="#sidebar-right-outlet" v-if="bioSettingsOpened">
+        <div class="sidebar">
+          <span class="text-2xl">Bio settings</span>
+        </div>
+      </Teleport>
+      <Teleport defer to="#sidebar-right-outlet" v-if="selectedWidgetIndex != null">
+        <div class="sidebar">
+          <span class="text-2xl">Widget settings</span>
+          <span>Widget-specific settings</span>
+          <span v-for="setting in SPECIFIC_SETTINGS_DEFINITIONS[widgetList[selectedWidgetIndex].type]" :key="setting.name">
+            <span class="text-zinc-400">{{ setting.name }}</span>
+            <Input type="text"
+              v-model="widgetList[selectedWidgetIndex].specificSettings[setting.name]" />
+          </span>
+          <span>Generic settings</span>
+          <span v-for="setting in GENERIC_SETTINGS_DEFINITIONS" :key="setting.name">
+            <span class="text-zinc-400">{{ setting.name }}</span>
+            <Input type="text"
+              v-model="widgetList[selectedWidgetIndex].genericSettings[setting.name]" />
+          </span>
+        </div>
+      </Teleport>
     <div id="widgets" class="w-[50%] flex flex-col gap-3 justify-center z-0">
-      <Widget v-for="(widget, i) in widgetList" :key="widget.id" :data="widget" @click="selectWidget(i)" />
+      <Widget v-for="(widget, i) in widgetList" :key="widget.id" :data="widget" @click="selectWidget(i)" :selected="i == selectedWidgetIndex"/>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+#widgets > *[selected=true] {
+  @apply border-2 border-dashed border-rose-500;
+}
+
+.sidebar {
+  @apply max-w-[400px] min-w-[300px] h-full bg-zinc-800/80 rounded-2xl z-[20] backdrop-blur-3xl relative p-4 flex flex-col gap-2 overflow-y-auto;
+
+  > span {
+    @apply mb-1;
+  }
+}
 </style>
