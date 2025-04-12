20dd3a951ad16bc622a025b0441629e986ff66fd
csandrisTurr <csaszarandras@turr.hu>
2025-04-13 01:04:57 +0200

    feat: removed abomination from icon component

--- src/components/global/Icon.vue
+++ src/components/global/Icon.vue
@@ -1,48 +1,15 @@
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
   type: string;
   size?: string;
 }>();
-
-const icons: Record<string, string> = {
-  check: 'check',
-  id_track: 'id_track',
-  widgets: 'widgets',
-  edit: 'edit',
-  settings: 'settings',
-  eye_tracking: 'eye_tracking',
-  house: 'house',
-  search: 'search',
-  delete: 'delete',
-  add: 'add',
-  add_2: 'add_2',
-  keyboard_arrow_down: 'keyboard_arrow_down',
-  arrow_drop_down: 'arrow_drop_down',
-  close: 'close',
-  close_small: 'close_small',
-  download: 'download',
-  more_vert: 'more_vert',
-  more_horiz: 'more_horiz',
-  apps: 'apps',
-  menu: 'menu',
-  person: 'person',
-  shield_person:'shield_person',
-  mail: "mail",
-  opened_mail: "drafts",
-  visibility: "visibility",
-  info: "info",
-  id_card: "id_card",
-  logout: "logout",
-  undo: "undo"
-};
 </script>
 
 <template>
   <span
-    v-if="icons[type]"
     class="material-symbols-outlined align-middle"
     :class="size ? `text-${size}` : 'text-lg'"
   >
-    {{ icons[type] }}
+    {{ type }}
   </span>
 </template>
