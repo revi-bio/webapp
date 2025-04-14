8663bb044c59f945062ce21f66d3053df7108317
Csoki <csokaandras@turr.hu>
2025-04-14 11:22:31 +0200

    feat: picture montage basics
    RBW-81

---
+++ src/components/widget/PicMontage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PicBox from '../global/PicBox.vue';
+import Button from '../global/Button.vue';
+import { ref } from 'vue';
+
+const components = ref([1]);
+
+</script>
+<template>
+    <PicBox v-for="(item, index) in components" :key="index"></PicBox>
+</template>
