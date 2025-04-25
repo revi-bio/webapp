aa183d501b5244d7c819cb20074c9b69381e8f94
csokaandras <csokaandras@turr.hu>
2025-04-25 16:47:29 +0200

    fix: delete default image from gallery

--- src/types/widgets/Gallery.ts
+++ src/types/widgets/Gallery.ts
@@ -17,7 +17,7 @@ export class GalleryWidget extends Widget {
       type: 'gallery',
       specificSettings: {
         title: 'This is a gallery widget',
-        images: ['http://65.87.7.245/file/680b8599500cbc3d3b2c456b'],
+        images: [],
       },
       genericSettings: options.genericSettings ?? {},
     });
