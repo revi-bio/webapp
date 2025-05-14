5daeeb32bed4e1fc506c2df6089d2c72ab302434
TatraiDominik <tatraidominikoszkar@turr.hu>
2025-05-14 11:35:39 +0200

    ref: layout and responsiveness on sidebar and settings

--- src/components/global/Sidebar.vue
+++ src/components/global/Sidebar.vue
@@ -38,21 +38,14 @@ const countOfUnreaded = computed(() => msgs.value.filter((msg) => !msg.isRead).l
 const mobileMenuOpen = ref<boolean>(false);
 
 const toggleMenu = async () => {
-  const mobileMenu = document.querySelector('.mobileMenu');
-
   if (mobileMenuOpen.value) {
-    // Bezárás animáció
-    if (mobileMenu) {
-      await animate(mobileMenu, { y: [0, 20], opacity: [1, 0] }, { duration: 0.2 });
-    }
     mobileMenuOpen.value = false;
   } else {
-
     mobileMenuOpen.value = true;
     await nextTick();
-    const refreshedMobileMenu = document.querySelector('.mobileMenu');
-    if (refreshedMobileMenu) {
-      animate(refreshedMobileMenu, { y: [20, 0], opacity: [0, 1] }, { duration: 0.2 });
+    const mobileMenu = document.querySelector('.mobileMenu');
+    if (mobileMenu) {
+      await animate(mobileMenu, { y: [20, 0] }, { duration: 0.2 });
     }
   }
 }
@@ -120,9 +113,9 @@ const toggleMenu = async () => {
   </div>
 
   <!--Bottom bar-->
-  <div class="w-full md:hidden flex flex-col justify-center content-center items-center">
+  <div class="w-full md:hidden flex flex-col justify-center content-center items-center absolute bottom-0 z-[100] mobileMenu">
 
-    <div class="w-full p-2 md:hidden flex flex-row justify-evenly content-center items-center bg-zinc-900">
+    <div class="w-full p-2 md:hidden flex flex-row justify-evenly content-center items-center bg-zinc-900 transition-transform duration-300">
       <span @click.prevent="toggleMenu" class="py-1 px-3 flex justify-center content-center items-center rounded-md bg-zinc-950 hover:bg-zinc-950/80 active:bg-zinc-800 transition-colors duration-200">
         <Icon :type="mobileMenuOpen ? 'close': 'menu'"></Icon>
       </span>
@@ -132,9 +125,22 @@ const toggleMenu = async () => {
         class="w-[48px] h-[48px] cursor-pointer"
         :avatar-url="userStore.getUserData().avatar"
       ></Avatar>
+
+      <div
+        v-if="toggleLogout"
+        class="absolute border-[1px] border-zinc-700 bottom-[4rem] right-2 transition duration-200 bg-zinc-800 hover:text-zinc-300 active:text-zinc-300 hover:bg-red-500 active:bg-red-600 p-3 rounded-md z-[200] text-red-500 w-[200px]"
+      >
+        <div
+          class="flex flex-row justify-between items-center cursor-pointer"
+          @click.stop="logoutUser()"
+        >
+          <h3 class="text-base">Log out</h3>
+          <Icon type="logout"></Icon>
+        </div>
+      </div>
     </div>
 
-    <div v-if="mobileMenuOpen" class="mobileMenu w-full p-4 md:hidden flex flex-row flex-wrap justify-evenly content-center items-center bg-zinc-950 gap-2 text-zinc-400 z-[100]">
+    <div v-if="mobileMenuOpen" class=" w-full p-4 gap-4 md:hidden flex flex-row flex-wrap justify-evenly content-center items-center bg-zinc-950 text-zinc-400 z-[100] transition-all duration-300">
       <div class="w-full flex flex-row justify-evenly content-center items-center">
         <RouterLink to="/baseDash/overview" class="navItem" @click="toggleMenu">
           <span>
@@ -153,17 +159,19 @@ const toggleMenu = async () => {
         </RouterLink>
       </div>
 
-      <RouterLink to="/baseDash/settings" class="navItem" @click="toggleMenu">
-        <span>
-          <Icon size="3xl" type="settings"></Icon>
-        </span>
-      </RouterLink>
-      <RouterLink to="/baseDash/inbox" class="navItem" @click="toggleMenu">
-        <span>
-          <Icon v-if="countOfUnreaded <= 0" size="3xl" type="mail"></Icon>
-          <Icon v-if="countOfUnreaded > 0" size="3xl" type="mark_email_unread"></Icon>
-        </span>
-      </RouterLink>
+      <div class="w-full flex flex-row justify-evenly content-center items-center">
+        <RouterLink to="/baseDash/settings" class="navItem" @click="toggleMenu">
+          <span>
+            <Icon size="3xl" type="settings"></Icon>
+          </span>
+        </RouterLink>
+        <RouterLink to="/baseDash/inbox" class="navItem" @click="toggleMenu">
+          <span>
+            <Icon v-if="countOfUnreaded <= 0" size="3xl" type="mail"></Icon>
+            <Icon v-if="countOfUnreaded > 0" size="3xl" type="mark_email_unread"></Icon>
+          </span>
+        </RouterLink>
+      </div>
     </div>
 
   </div>
