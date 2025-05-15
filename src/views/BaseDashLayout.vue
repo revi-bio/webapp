3410d3e34ea35573211fbe574a30e5437dd4a36c
TatraiDominik <tatraidominikoszkar@turr.hu>
2025-05-15 19:56:01 +0200

    ref: padding bottom added when bottombar is visible so we can see everything

--- src/views/BaseDashLayout.vue
+++ src/views/BaseDashLayout.vue
@@ -30,7 +30,7 @@ onMounted(() => {
     class="flex flex-col-reverse md:flex-row justify-center content-center items-center w-full h-full md:p-6 gap-6 relative overflow-hidden">
     <Sidebar class="w-full md:w-[80px] h-full"></Sidebar>
     <main id="sidebar-right-outlet" class="w-full h-full relative rounded-2xl z-10 flex gap-4">
-      <div class="w-full h-full md:bg-zinc-800/80 rounded-2xl z-[20] md:backdrop-blur-3xl relative overflow-clip">
+      <div class="w-full h-full md:bg-zinc-800/80 rounded-2xl z-[20] md:backdrop-blur-3xl pb-16 md:pb-0 relative overflow-clip">
         <!--Content goes here-->
         <span class="absolute text-3xl top-6 left-6"
           ><h3>{{ route.name }}</h3></span
@@ -38,7 +38,6 @@ onMounted(() => {
         <RouterView></RouterView>
       </div>
     </main>
-    <!--Blob svg - Egységes arányos pozicionálás és méret-->
     <svg
       class="blob origin-center absolute
              bottom-[-30vh] right-[-20vw]
